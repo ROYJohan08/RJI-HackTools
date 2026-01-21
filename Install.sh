4a6e2621d29c0be9020ed1f29a692efbcb0176ed
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2026-01-21 16:39:13 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -13,6 +13,15 @@ sudo mkdir -p /home/kali/tools
 cd /home/kali/tools
 sudo git clone https://github.com/danielmiessler/SecLists
 
+git clone https://github.com/megadose/holehe.git
+cd holehe/
+python3 setup.py install
+cd ../
+
+git clone https://github.com/smicallef/spiderfoot.git
+cd spiderfoot
+python3 -m pip install -r requirements.txt
+cd ../
 
 sudo wget https://github.com/ROYJohan08/RJI-HackTools/raw/main/.zshrc
 sudo rm -rf /home/kali/.zshrc
